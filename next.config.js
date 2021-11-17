64d065dce55009cc62cd3dd77b699b6e03fa1b99
pr_Mais <hiimechi@gmail.com>
2021-11-17 12:58:31 +0300

    feat: localization and ar+en translation files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  disDir: 'out',
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'ar',
+    localeDetection: false,
+  },
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function WTMWebsite({ Component, pageProps }: AppProps) {
+  const {locale} = useRouter()
+  return <div dir={locale === 'ar' ? 'rtl' : 'ltr'}>
+    <Component  {...pageProps} />
+  </div>
 }
 
-export default MyApp
+export default WTMWebsite
