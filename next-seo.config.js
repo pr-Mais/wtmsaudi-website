b3bad62cda3d761c0b472b698f45980f4d3785ac
pr_Mais <hiimechi@gmail.com>
2021-11-17 14:15:08 +0300

    feat: add next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,20 @@
+export default {
+      openGraph: {
+            type: 'website',
+            url: 'wtmsaudi.com',
+            site_name: 'WTM Saudi',
+            images: [
+                  {
+                        url: 'https://images.pexels.com/photos/10243822/pexels-photo-10243822.jpeg?cs=srgb&dl=pexels-диана-дунаева-10243822.jpg',
+                        width: 800,
+                        height: 600,
+                        alt: 'WTM Saudi Header Image',
+                  }
+            ]
+      },
+      twitter: {
+            handle: '@WTMSaudi',
+            cardType: 'summary_large_image',
+
+      },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
+import { DefaultSeo } from 'next-seo';
+import SEO from '../next-seo.config';
 
 function WTMWebsite({ Component, pageProps }: AppProps) {
   const {locale} = useRouter()
   return <div dir={locale === 'ar' ? 'rtl' : 'ltr'}>
+    <DefaultSeo {...SEO} />
     <Component  {...pageProps} />
   </div>
 }
