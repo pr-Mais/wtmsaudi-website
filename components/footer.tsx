afe9f2d0fa04f53655fff958c3defbebb5c01c7f
pr_Mais <hiimechi@gmail.com>
2021-11-20 13:23:07 +0300

    feat: footer sections

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,16 +4,29 @@ import Link from 'next/link';
 
 const Footer = ({ ...pageProps }: Params) => {
   return (
-    <div className="bg-blue footer h-52">
+    <div className="bg-blue footer h-auto">
       <div className="max-w-5xl mx-auto px-4 sm:px-6 ">
         <div className="flex justify-between items-top py-7">
-          <div className="text-white">{pageProps.lngDict.name.title}</div>
-          <div className="flex justify-end m-l-0">
+          <div className="flex flex-col pt-2">
+            <div className="text-white text-xl pb-4">مجموعات مطوري Google</div>
+            <div className="text-white font-thin pb-2">مجتمع Google السحابي السعودي</div>
+            <div className="text-white font-thin pb-2">أندية الطلبة المطورين</div>
+            <div className="text-white font-thin pb-2">خبراء مطوري Google</div>
+          </div>
+          <div className="flex flex-col pt-2">
+            <div className="text-white text-xl pb-4">مصادر مفيدة</div>
+            <div className="text-white font-thin pb-2">اكتب في مدونتنا</div>
+            <div className="text-white font-thin pb-2">تقديم طلب انضمام كمتحدثة</div>
+            <div className="text-white font-thin pb-2">كن راعياً لنا</div>
+          </div>
+          <div>
+            {' '}
             <Link href="/">
               <Image src="/logo-white.svg" alt="WTMSaudi Logo White" width="300" height="50" />
             </Link>
           </div>
         </div>
+        <div className="text-white font-thin py-8 justify-center flex">copyright </div>
       </div>
     </div>
   );
