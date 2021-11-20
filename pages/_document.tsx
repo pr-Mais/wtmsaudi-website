5c540844a125618d2de50dd46f1c214dad40164f
pr_Mais <hiimechi@gmail.com>
2021-11-20 16:14:27 +0300

    fix: font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript, Html } from "next/document";
+import Document, { Head, Main, NextScript, Html } from 'next/document';
 
 /* -------------------------------------------------------------------------- */
 /*                                  DOCUMENT                                  */
@@ -10,63 +10,25 @@ export default class NextDocument extends Document {
 
     const props = this.props.__NEXT_DATA__;
 
-    const dir = props.locale === "ar" ? "rtl" : "ltr";
-    const lang = props.locale || "ar";
+    const dir = props.locale === 'ar' ? 'rtl' : 'ltr';
+    const lang = props.locale || 'ar';
 
     return (
-      <Html lang={lang} dir={dir} className="antialiased rtl:font-dubai">
+      <Html lang={lang} dir={dir} className="antialiased">
         <Head>
-          <link
-            rel="preload"
-            href="/fonts/Dubai-Bold.ttf"
-            as="font"
-            type="font/ttf"
-            crossOrigin="true"
-          />
-          <link
-            rel="preload"
-            href="/fonts/Dubai-Light.ttf"
-            as="font"
-            type="font/ttf"
-            crossOrigin="true"
-          />
-          <link
-            rel="preload"
-            href="/fonts/Dubai-Medium.ttf"
-            as="font"
-            type="font/ttf"
-            crossOrigin="true"
-          />
-          <link
-            rel="preload"
-            href="/fonts/Dubai-Regular.ttf"
-            as="font"
-            type="font/ttf"
-            crossOrigin="true"
-          />
+          <link rel="preload" href="/fonts/Dubai-Bold.ttf" as="font" type="font/ttf" crossOrigin="true" />
+          <link rel="preload" href="/fonts/Dubai-Light.ttf" as="font" type="font/ttf" crossOrigin="true" />
+          <link rel="preload" href="/fonts/Dubai-Medium.ttf" as="font" type="font/ttf" crossOrigin="true" />
+          <link rel="preload" href="/fonts/Dubai-Regular.ttf" as="font" type="font/ttf" crossOrigin="true" />
 
           <link rel="icon" href="/favicon/favicon.ico" />
 
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-          />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
           <link rel="manifest" href="/favicon/site.webmanifest" />
         </Head>
-        <body dir={dir} id="body">
+        <body dir={dir} className="rtl:font-dubai">
           <Main />
           <NextScript />
         </body>
